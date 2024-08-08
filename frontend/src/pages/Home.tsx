f8df46e3bced430cac7dc800f371b9a5692ce43f
filipzupancic <filipzupancic123@gmail.com>
2024-08-08 12:29:33 +0200

    New name, use NGINX

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -9,10 +9,10 @@ export const Home = () => {
                     <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
                         <div className="text-center">
                             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                                OTC Desk
+                                Pear AMM
                             </h1>
                             <p className="mt-6 text-lg leading-8 text-gray-600">
-                                OTC Desk is a peer-to-peer trading platform for cryptocurrencies with no intermediaries.
+                                A peer-to-peer trading platform for cryptocurrencies with no intermediaries.
                             </p>
                             <div className="mt-10 flex items-center justify-center gap-x-6">
                                 <a
